e71ac98b90dd4eaef65fc339413abc6d3362e794
Pierre VAZ <vaz.pierre57@gmail.com>
2023-06-12 13:43:39 +0200

    cs

--- src/app/pages/alarmes/alarmes.page.ts
+++ src/app/pages/alarmes/alarmes.page.ts
@@ -3,6 +3,8 @@ import {appInjector} from '../../app.module';
 import {AlarmeModule} from '../../modules/alarme.module';
 import {ALARMES} from '../../../data/alarmes';
 import {Storage} from '@ionic/storage-angular';
+import { AlertController } from '@ionic/angular';
+
 
 const storage = appInjector.get(Storage);
 
@@ -14,14 +16,16 @@ const storage = appInjector.get(Storage);
 export class AlarmesPage implements OnInit {
   public alarmeModule = AlarmeModule.getInstance();
   public modifier = '/modifier/';
+  public alertController: AlertController;
 
   async ngOnInit() {
     await storage.create();
     this.alarmeModule.initAlarme();
+    this.alertController = appInjector.get(AlertController);
   }
 
-  supprimer(alarm) {
-    this.alarmeModule.removeAlarme(alarm.id);
+  async supprimer(alarm) {
+    await this.afficherConfirmation('Voulez-vous supprimer l\'alarme : '+ alarm.nom + ' ?', 'supprimer', alarm);
   }
 
   allumer(alarm) {
@@ -34,4 +38,33 @@ export class AlarmesPage implements OnInit {
     this.alarmeModule.addAlarme(this.alarmeModule.sample());
   }
 
+  async afficherConfirmation(_message: string, _action: string, arg: any) {
+    const alert = await this.alertController.create({
+      header: 'Confirmation',
+      message: _message,
+      buttons: [
+        {
+          text: 'Annuler',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            // Action à effectuer lorsque l'utilisateur clique sur "Annuler"
+            console.log('Action annulée');
+          }
+        },
+        {
+          text: 'Confirmer',
+          handler: () => {
+            if(_action === 'supprimer'){
+              this.alarmeModule.removeAlarme(arg.id);
+            }
+            // Action à effectuer lorsque l'utilisateur clique sur "Confirmer"
+            console.log('Action confirmée');
+            // Ajoutez ici le code pour effectuer l'action souhaitée
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
 }
